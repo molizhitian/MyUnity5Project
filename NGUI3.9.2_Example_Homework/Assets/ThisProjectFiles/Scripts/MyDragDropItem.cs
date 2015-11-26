aa8d8bc8f735510c8bec4decada316e48f3aeffb
molizhitian <228025204@qq.com>
2015-11-26 13:21:30 +0800

    knapsack

---
+++ NGUI3.9.2_Example_Homework/Assets/ThisProjectFiles/Scripts/MyDragDropItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyDragDropItem : UIDragDropItem {
+
+    protected override void OnDragDropRelease(GameObject surface)
+    {
+        base.OnDragDropRelease(surface);
+
+        Debug.Log(surface);
+
+        //居中显示
+        this.transform.parent = surface.transform;
+        this.transform.localPosition = Vector3.zero;
+    }
+
+}
