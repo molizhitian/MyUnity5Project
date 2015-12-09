701f0eefe630fc2ff5defceffbf79d5914bbffaf
molizhitian <228025204@qq.com>
2015-12-09 22:49:36 +0800

    Add Knapsack Function20151209
    Knapsack Drag And Drop PickUp
    keyCode P is Random a Item

--- NGUI3.9.2_Example_Homework/Assets/ThisProjectFiles/Scripts/MyDragDropItem.cs
+++ NGUI3.9.2_Example_Homework/Assets/ThisProjectFiles/Scripts/MyDragDropItem.cs
@@ -3,15 +3,44 @@ using System.Collections;
 
 public class MyDragDropItem : UIDragDropItem {
 
+    public UISprite sprite;
+    public UILabel label;
+    public int count = 1;
+
+    //这么写就是当参数没有的时候默认传的是1
+    public void AddCount(int number = 1)
+    {
+        count += number;
+        label.text = count.ToString();
+    }
+
+
     protected override void OnDragDropRelease(GameObject surface)
     {
         base.OnDragDropRelease(surface);
 
         Debug.Log(surface);
 
-        //居中显示
-        this.transform.parent = surface.transform;
-        this.transform.localPosition = Vector3.zero;
+        if (surface.tag == "Cell")
+        {
+            //居中显示
+            this.transform.parent = surface.transform;
+            this.transform.localPosition = Vector3.zero;
+        }
+        else if (surface.tag == "MyDragDropItem")
+        {
+            Transform parent = surface.transform.parent;
+            surface.transform.parent = this.transform.parent;
+            surface.transform.localPosition = Vector3.zero;
+
+            this.transform.parent = parent;
+            this.transform.localPosition = Vector3.zero;
+        }
+        
+
+
+        
+        
     }
 
 }
